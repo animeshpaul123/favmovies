e9538142771a826fdb804dbd9ca5aa217b724980
Animesh Paul <animeshpaul341@gmail.com>
2020-09-10 20:56:36 +0530

    Updates

---
+++ precache-manifest.6e98838cf6b69353acf9188538fee2ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e247011db8f5c652cdd85f19f8804df",
+    "url": "/favmovies/index.html"
+  },
+  {
+    "revision": "4540f32012912d74a352",
+    "url": "/favmovies/static/css/2.c37a399a.chunk.css"
+  },
+  {
+    "revision": "ad25cb72c3544a2b4587",
+    "url": "/favmovies/static/css/main.15070f6e.chunk.css"
+  },
+  {
+    "revision": "4540f32012912d74a352",
+    "url": "/favmovies/static/js/2.6f1cd7c6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/favmovies/static/js/2.6f1cd7c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad25cb72c3544a2b4587",
+    "url": "/favmovies/static/js/main.110ea334.chunk.js"
+  },
+  {
+    "revision": "bddca9d25406ac2f2d64",
+    "url": "/favmovies/static/js/runtime-main.e1f69e81.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/favmovies/static/media/logo.5d5d9eef.svg"
+  }
+]);
